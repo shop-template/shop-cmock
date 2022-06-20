feb27d5d7d3f4ed3567026a6d21b60faac8f29e8
fxss <723107234@qq.com>
2022-06-20 23:01:50 +0800

    feat: 用户信息

---
+++ mocks/_api_userInfo.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"name": "用户信息",
+	"url": "/api/userInfo",
+	"method": "post",
+	"type": "application/json",
+	"createTime": "2022-06-20 22:53:46",
+	"updateTime": "2022-06-20 22:56:28",
+	"isUseMockjs": false,
+	"timeout": 1500,
+	"bodyKey": {},
+	"body": {
+		"default": {
+			"id": 6,
+			"name": "fxss",
+			"account": "182****9555",
+			"headerImg": "https://img.fxss.work/header-1583418772000-23-production"
+		}
+	}
+}
